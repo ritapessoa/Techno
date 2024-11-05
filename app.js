7ca722b27fd439624ecfadba8af5dcd37018d530
ritapessoa <ritapessoarosa@gmail.com>
2024-11-05 16:16:11 -0300

    Modal adicionado

--- app.js
+++ app.js
@@ -27,6 +27,18 @@ const vm = new Vue({
           this.produto = r;
         });
     },
+    abrirModal(id) {
+      this.fetchProduto(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    },
+    fecharModal({ target, currentTarget }) {
+      if (target === currentTarget) {
+        this.produto = false;
+      }
+    },
   },
   created() {
     this.fetchProdutos();
